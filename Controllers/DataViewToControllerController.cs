4b669d6e1e19c3b3f815f056ebf7dc5a6a3a1ad3
Shubham <shubhamd@bioenabletech.com>
2022-12-06 17:26:31 +0530

    data from view to controller

---
+++ Controllers/DataViewToControllerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestProject_MVC.Controllers
+{
+    public class DataViewToControllerController : Controller
+    {
+        // GET: DataViewToControlle
+        [Route("viewToController")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string UsingParameter(int EmployeeNo, string EmployeeName)
+        {
+            return "Data from parameters - ID :" + EmployeeNo + ", Name : " + EmployeeName;
+        }
+
+        [HttpPost]
+        public string UsingRequest()
+        {
+            return "Data from parameters - ID :" + Request["EmployeeNo"] + ", Name : " + Request["EmployeeName"];
+        }
+    }
+}
