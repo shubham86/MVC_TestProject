d9a8c414a66be91b59923530b80327166ab87901
Shubham <shubhamd@bioenabletech.com>
2022-12-12 18:33:24 +0530

    insert data in DB using entityframework

---
+++ Controllers/InsertDataController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_MVC.Model;
+using Test_MVC.db.dbOperations;
+
+namespace TestProject_MVC.Controllers
+{
+    public class InsertDataController : Controller
+    {
+        public InsertDataController()
+        { 
+            
+        }
+
+
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        public ActionResult addEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult addEmployee(EmploayeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = employeeRepository.AddEmployee(model);
+                if (id > 0)
+                {
+                    ModelState.Clear();
+                }
+            }
+            return View();
+        }
+    }
+}
