115b9ec9fa5fae2c3f7a16068a85e896475bc368
Shubham <shubhamd@bioenabletech.com>
2023-01-02 09:35:59 +0530

    update and delete record using entity states

--- Controllers/InsertDataController.cs
+++ Controllers/InsertDataController.cs
@@ -51,5 +51,40 @@ namespace TestProject_MVC.Controllers
             var result = employeeRepository.getEmployee(id);
             return View(result);
         }
+
+
+        [HttpGet]
+        public ActionResult editEmployee(int id)
+        {
+            var result = employeeRepository.getEmployee(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult editEmployee(EmploayeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isEdit = employeeRepository.UpdateEmployee(model.employeeID, model);
+
+                if (isEdit)
+                {
+                    return RedirectToAction("getAllEmployees");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult deleteEmployee(int id)
+        {
+            bool isDelete = employeeRepository.RemoveEmployee(id);
+
+            if (isDelete)
+            {
+                return RedirectToAction("getAllEmployees");
+            }
+
+            return View();
+        }
     }
 }
